197f9ef76680b47b1204189238761eae2aa20b12
santhisri kh <52741412+santhisrikh@users.noreply.github.com>
2020-12-05 14:43:41 +0530

    Update AddQty.jsx

--- Module4/Sprint 1/GroceryList/src/components/AddQty.jsx
+++ Module4/Sprint 1/GroceryList/src/components/AddQty.jsx
@@ -20,6 +20,7 @@ class AddQty extends React.Component{
     render(){
         return(
             <div style = {{display:"flex"}}>
+<!--   we can use diabled property of button  to make the (-) button inactive if the qty bexomes Zero.              -->
                 <button disabled = {this.state.count<=0} onClick = {this.handleReduce}>-</button>
                 <p style = {{margin:"2px"}}>{this.state.count}</p>
                 <button onClick = {this.handleAdd}>+</button>
